8d819a4f23b079933282094dff1d445c4ba584e8
luizhp <luizhp@yahoo.com>
2020-10-04 15:37:11 -0300

    chore: initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'template-app-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'template-app-angular';
+}
